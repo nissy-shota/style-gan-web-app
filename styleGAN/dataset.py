92c38a2b5d18881b5d8a0f3f19bc42fc7199ea39
nissy-shota <shota_4410@icloud.com>
2021-05-30 23:17:53 +0900

    dataset

--- styleGAN/dataset.py
+++ styleGAN/dataset.py
@@ -1,64 +1,23 @@
 from __future__ import print_function
-
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-
 from PIL import Image
-import matplotlib.pyplot as plt
-
 import torchvision.transforms as transforms
-import torchvision.models as models
 
-import copy
+class ImageDataSet():
 
+    def __init__(self, device):
+        self.device = device
+        # desired size of the output image
+        self.imsize = 512 if torch.cuda.is_available() else 128  # use small size if no gpu
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# desired size of the output image
-imsize = 512 if torch.cuda.is_available() else 128  # use small size if no gpu
-
-loader = transforms.Compose([
-    transforms.Resize(imsize),  # scale imported image
-    transforms.ToTensor()])  # transform it into a torch tensor
-
-
-def image_loader(image_name):
-    image = Image.open(image_name)
-    # fake batch dimension required to fit network's input dimensions
-    image = loader(image).unsqueeze(0)
-    return image.to(device, torch.float)
+        self.loader = transforms.Compose([
+            transforms.Resize(self.imsize),  # scale imported image
+            transforms.ToTensor()])  # transform it into a torch tensor
 
 
-style_img = image_loader("./data/images/neural-style/picasso.jpg")
-content_img = image_loader("./data/images/neural-style/dancing.jpg")
+    def image_loader(self, image_name):
 
-assert style_img.size() == content_img.size(), \
-    "we need to import style and content images of the same size"
-
-
-######################################################################
-# Now, let's create a function that displays an image by reconverting a
-# copy of it to PIL format and displaying the copy using
-# ``plt.imshow``. We will try displaying the content and style images
-# to ensure they were imported correctly.
-
-unloader = transforms.ToPILImage()  # reconvert into PIL image
-
-plt.ion()
-
-def imshow(tensor, title=None):
-    image = tensor.cpu().clone()  # we clone the tensor to not do changes on it
-    image = image.squeeze(0)      # remove the fake batch dimension
-    image = unloader(image)
-    plt.imshow(image)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001) # pause a bit so that plots are updated
-
-
-plt.figure()
-imshow(style_img, title='Style Image')
-
-plt.figure()
-imshow(content_img, title='Content Image')
+        image = Image.open(image_name)
+        # fake batch dimension required to fit network's input dimensions
+        image = self.loader(image).unsqueeze(0)
+        return image.to(self.device, torch.float)
